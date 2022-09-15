60be47ff7afd3823bcdabcce022fc6f9ee3787b2
Alexis Haldy <alexishaldy@gmail.com>
2022-09-15 10:22:31 +0200

    Improve variable names used
    Add comments to functions

--- Poker/models/deck/deck.cpp
+++ Poker/models/deck/deck.cpp
@@ -1,26 +1,32 @@
 #include "deck.h"
+
+#include <random>
+
 #include "../utility/utils.h"
 
-constexpr int HAND_SIZE = 5;
 const std::vector<Suit> HANDS = {Suit::CLUBS, Suit::DIAMONDS, Suit::HEARTS, Suit::SPADES};
 const std::vector<Value> VALUES = { Value::TWO, Value::THREE, Value::FOUR, Value::FIVE, Value::SIX, Value::SEVEN, Value::EIGHT, Value::NINE, Value::TEN, Value::JACK, Value::QUEEN, Value::KING, Value::ACE };
 
 Deck::Deck()
 {
-	this->_cards = {};
-	for (int hand = static_cast<int>(Suit::CLUBS); hand < static_cast<int>(Suit::END); hand++)
+	// Loop in suits and values to insert every type of cards inside the deck
+	for (int suit = static_cast<int>(Suit::CLUBS); suit < static_cast<int>(Suit::END); suit++)
 	{
 		for (int value = static_cast<int>(Value::TWO); value < static_cast<int>(Value::END); value++)
 		{
-			this->_cards.emplace_back(Card(static_cast<Suit>(hand), static_cast<Value>(value)));
+			this->_cards.emplace_back(Card(static_cast<Suit>(suit), static_cast<Value>(value)));
 		}
 	}
 }
 
 Card Deck::PickRandomCard()
 {
-	int randomIndex = Utils::GetRandomInt(0, static_cast<int>(this->_cards.size()) - 1);
-	Card card = this->_cards[randomIndex];
-	//TODO: Remove card from deck
+	// Get a random int between 0 and the size of the deck
+	std::shuffle(this->_cards.begin(), this->_cards.end(), std::random_device());
+	// Get a card from the back of the list
+	const Card card = this->_cards.back();
+	// Remove the card from the deck
+	Utils::Remove(this->_cards, card);
+
 	return card;
 }
