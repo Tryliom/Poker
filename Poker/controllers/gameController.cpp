530278c95479a6e03ae27e5c382e045af032941d
Alexis Haldy <alexishaldy@gmail.com>
2022-09-15 08:17:52 +0200

    Declare function in GameController

--- Poker/controllers/gameController.cpp
+++ Poker/controllers/gameController.cpp
@@ -1,1 +1,18 @@
 #include "gameController.h"
+
+GameController::GameController()
+{
+	this->_deck = Deck();
+}	
+
+
+void GameController::Start()
+{
+	
+}
+
+void GameController::Restart()
+{
+	this->_deck = Deck();
+}
+
--- Poker/controllers/gameController.h
+++ Poker/controllers/gameController.h
@@ -1,6 +1,16 @@
 #pragma once
+#include "../models/deck/deck.h"
+
+int constexpr NB_PLAYER = 5;
+int constexpr START_CARD_PER_PLAYERS = 5;
+
 class GameController
 {
-
+private:
+	Deck _deck;
+public:
+	GameController();
+	void Start();
+	void Restart();
 };
 
