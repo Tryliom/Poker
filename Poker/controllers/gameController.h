5f910eb176c435466f9c268b5c12e62e1e09122a
Alexis Haldy <alexishaldy@gmail.com>
2022-09-15 09:54:15 +0200

    Add comment to GameController

--- Poker/controllers/gameController.h
+++ Poker/controllers/gameController.h
@@ -7,10 +7,19 @@ int constexpr START_CARD_PER_PLAYERS = 5;
 class GameController
 {
 private:
+	/**
+	 * \brief The whole deck used for a game
+	 */
 	Deck _deck;
 public:
 	GameController();
+	/**
+	 * \brief Start the game
+	 */
 	void Start();
+	/**
+	 * \brief Restart the game, reset the deck and all other attribute used in the game
+	 */
 	void Restart();
 };
 
