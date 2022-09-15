070791cc4c222cc40c151afd6b64985b407f0822
Alexis Haldy <alexishaldy@gmail.com>
2022-09-15 16:53:43 +0200

    Refactor handValue to pattern

---
+++ Poker/models/deck/pattern/pattern.cpp
@@ -0,0 +1,131 @@
+﻿#include "Pattern.h"
+
+#include <iostream>
+#include <map>
+#include <ostream>
+
+Pattern::Pattern()
+{
+	this->_patternType = PatternType::END;
+	this->_bestValues = {CardValue::END};
+}
+
+Pattern::Pattern(const PatternType patternType, const std::vector<CardValue>& bestValues, CheckPriority checkPriority)
+{
+	this->_patternType = patternType;
+	this->_bestValues = bestValues;
+	this->_checkPriority = checkPriority;
+}
+
+Pattern::Pattern(const PatternType patternType, const CardValue& bestValue, CheckPriority checkPriority)
+{
+	this->_patternType = patternType;
+	this->_bestValues = { bestValue };
+	this->_checkPriority = checkPriority;
+}
+
+bool Pattern::IsBetter(const Pattern& other) const
+{
+	if (this->_patternType == other._patternType)
+	{
+		for (int i = 0; i < static_cast<int>(this->_bestValues.size()); i++)
+		{
+			if (this->_bestValues.at(i) > other._bestValues.at(i))
+			{
+				return true;
+			}
+		}
+		//TODO: Return an enum in order to count a draw
+		return false;
+	}
+
+	return this->_patternType > other._patternType;
+}
+
+PatternType Pattern::GetPatternType() const
+{
+	return this->_patternType;
+}
+
+std::vector<CardValue> Pattern::GetCardValue() const
+{
+	return this->_bestValues;
+}
+
+Pattern Check(std::vector<Card>& cards)
+{
+	//TODO: Check in PatternType reverse order for all types
+
+    std::map<CardValue, int> sameCardValue = {};
+    std::map<CardSuit, int> sameCardSuit = {};
+    std::map<CardSuit, std::map<CardValue, int>> sortedCards = {};
+    std::vector<CardValue> pair = {};
+    Pattern pattern;
+
+
+    for (Card card : cards)
+    {
+        if (sameCardValue.find(card.GetValue()) != sameCardValue.end())
+        {
+            sameCardValue[card.GetValue()] = 0;
+        }
+        sameCardValue[card.GetValue()]++;
+
+        if (sameCardSuit.find(card.GetSuit()) != sameCardSuit.end())
+        {
+            sameCardSuit[card.GetSuit()] = 0;
+        }
+        sameCardSuit[card.GetSuit()]++;
+
+        if (sortedCards.find(card.GetSuit()) != sortedCards.end())
+        {
+            sortedCards[card.GetSuit()] = {};
+        }
+
+        if (sortedCards[card.GetSuit()].find(card.GetValue()) != sortedCards[card.GetSuit()].end())
+        {
+            sortedCards[card.GetSuit()][card.GetValue()] = 0;
+        }
+        sortedCards[card.GetSuit()][card.GetValue()]++;
+    }
+
+
+    for (const auto& element : sameCardValue)
+    {
+        std::cout << static_cast<int>(element.first) << " " << static_cast<int>(element.second) << std::endl;
+
+        if (element.second == 4)
+        {
+            pattern = Pattern(PatternType::FOUR_OF_A_KIND, element.first);
+        }
+        else if (element.second == 3)
+        {
+            if (pattern.GetPatternType() == PatternType::PAIR)
+            {
+                pattern = Pattern(PatternType::FULL_HOUSE, { element.first , pattern.GetCardValue().front() });
+            }
+            else
+            {
+                pattern = Pattern(PatternType::THREE_OF_A_KIND, element.first);
+            }
+        }
+        else if (element.second == 2)
+        {
+            if (pattern.GetPatternType() == PatternType::THREE_OF_A_KIND)
+            {
+                pattern = Pattern(PatternType::FULL_HOUSE, { pattern.GetCardValue().front(), element.first });
+            }
+            else if (pattern.GetPatternType() == PatternType::PAIR)
+            {
+                pattern = Pattern(PatternType::TWO_PAIRS, { pattern.GetCardValue().front(), element.first });
+            }
+            else
+            {
+                pattern = Pattern(PatternType::PAIR, element.first, CheckPriority::NO_ORDER);
+            }
+        }
+    }
+
+
+	return Pattern();
+}
---
+++ Poker/models/deck/pattern/pattern.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include "../card.h"
+
+#include <vector>
+
+enum class PatternType
+{
+	HIGHCARD,
+	PAIR,
+	TWO_PAIRS,
+	THREE_OF_A_KIND,
+	STRAIGHT,
+	FLUSH,
+	FULL_HOUSE,
+	FOUR_OF_A_KIND,
+	STRAIGHT_FLUSH,
+	END
+};
+
+enum class CheckPriority
+{
+	ORDER,
+	NO_ORDER
+};
+
+class Pattern
+{
+private:
+	PatternType _patternType;
+	std::vector<CardValue> _bestValues;
+	CheckPriority _checkPriority;
+public:
+	Pattern();
+	Pattern(const PatternType patternType, const std::vector<CardValue>& bestValues, CheckPriority checkPriority = CheckPriority::ORDER);
+	Pattern(const PatternType patternType, const CardValue& bestValue, CheckPriority checkPriority = CheckPriority::ORDER);
+	
+	PatternType GetPatternType() const;
+	std::vector<CardValue>  GetCardValue() const;
+	bool IsBetter(const Pattern& other) const;
+	static Pattern Check(const std::vector<Card>& cards);
+};
