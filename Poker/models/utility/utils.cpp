8765656b5aa70bd9414781775866cd8d767f6ebd
Alexis Haldy <alexishaldy@gmail.com>
2022-09-15 10:21:15 +0200

    Remove the random method
    Implement the remove method for vectors

--- Poker/models/utility/utils.cpp
+++ Poker/models/utility/utils.cpp
@@ -1,17 +1,7 @@
-#include <random>
+#include <cstdlib>
 
 namespace Utils
 {
-	int GetRandomInt(const int min, const int max) {
-		std::random_device osSeed;
-		const uint_least32_t seed = osSeed();
-
-		std::mt19937 generator(seed);
-		std::uniform_int_distribution<uint_least32_t> distribute(min, max);
-
-		return distribute(generator);
-	}
-
 	void ClearConsole() {
 		#if defined _WIN32
 			// For windows
